abc4b0a43489778f7fb4f99385f71cd9f6c4bc38
Daniel <danieloladele7@gmail.com>
2022-10-31 15:35:43 +0200

    test module and model module class added

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+
+    """Class for base model of object hierarchy."""
+    pass
---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for TestHBNBCommand class."""
+
+import unittest
+import datetime
+import sys
+from io import StringIO
+import re
+import os
+
+
+class TestALXCommand(unittest.TestCase):
+
+    """Tests ALXCommand console."""
+    pass
