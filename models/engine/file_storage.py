0d3c04c047f9cf7d37212ca10d528a9b71187db2
Daniel <danieloladele7@gmail.com>
2022-10-31 15:31:40 +0200

    model and engine added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for serializtion and deserialization of base classes."""
+    pass
