b557901862fba61fa7b4c51785bc0e35e1d47149
Daniel <danieloladele7@gmail.com>
2022-11-01 00:27:12 +0200

    engine and models update

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+
+
+class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    pass
